e663b7ba5d8e7221f3d30b1af378e9fe0cd41f55
Petr Svoboda <petr@jatys.cz>
2023-04-11 15:48:33 +0200

    Reformat

--- rise.nix
+++ rise.nix
@@ -1,16 +1,13 @@
-{
-  python,
-  ...
-}:
+{python, ...}:
 with python.pkgs;
-buildPythonPackage rec {
-  pname = "rise";
-  version = "5.7.1";
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-ZB23d8uQe/Xm3AUwmNf9ITgT+pqUZULlK5AOtwlSiaY=";
-  };
-  propagatedBuildInputs = [
-    notebook
-  ];
-}
+  buildPythonPackage rec {
+    pname = "rise";
+    version = "5.7.1";
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-ZB23d8uQe/Xm3AUwmNf9ITgT+pqUZULlK5AOtwlSiaY=";
+    };
+    propagatedBuildInputs = [
+      notebook
+    ];
+  }
