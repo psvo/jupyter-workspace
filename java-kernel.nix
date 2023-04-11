6d198d0af6456b23f419108140a376308333ab5c
Petr Svoboda <petr@jatys.cz>
2023-04-11 23:53:17 +0200

    Improve java-kernel package

--- java-kernel.nix
+++ java-kernel.nix
@@ -7,37 +7,32 @@
 python.pkgs.buildPythonPackage rec {
   pname = "java-kernel";
   version = "1.3.0";
-  preBuild = ''
-    cat > setup.py <<-EOF
-    from setuptools import setup
 
-    #with open('requirements.txt') as f:
-    #    install_requires = f.read().splitlines()
-
-    setup(
-      name='ijava-kernel',
-      #version='0',
-    )
-    EOF
-  '';
   src = fetchzip {
     url = "https://github.com/SpencerPark/IJava/releases/download/v${version}/ijava-${version}.zip";
     sha256 = "sha256-duuJC5iUz1jj1xshHjhEoa8lb5YmPjOMz42naFrGGCU=";
     stripRoot = false;
   };
 
-  propagatedBuildInputs = [
-    jdk
-  ];
+  buildInputs = [jdk];
+  nativeBuildInputs = [python.pkgs.notebook];
 
-  nativeBuildInputs = [
-    python.pkgs.notebook
-  ];
+  prePatch = ''
+    cat > setup.py <<-EOF
+    from setuptools import setup
+    setup(
+      name='java-kernel',
+    )
+    EOF
+  '';
 
   doCheck = false;
-  dontConfigure = true;
 
   installPhase = ''
     ${python.interpreter} install.py --prefix $out
+  '';
+
+  fixupPhase = ''
+    substituteInPlace "$out/share/jupyter/kernels/java/kernel.json" --replace '"java"' '"${jdk}/bin/java"'
   '';
 }
